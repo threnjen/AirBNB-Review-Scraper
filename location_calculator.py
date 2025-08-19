21b0fdbed0d0467cf792c506aeadc20496c59fe3
threnjen <threnjen@gmail.com>
2025-08-19 14:15:59 -0700

    format everything with black

--- location_calculator.py
+++ location_calculator.py
@@ -5,18 +5,22 @@ postal_code = "97224"
 iso_code = "us"
 
 
-def locationer(postal_code, iso_code= "us"):
+def locationer(postal_code, iso_code="us"):
 
     try:
         nomi = pgeocode.Nominatim(iso_code)
         query = nomi.query_postal_code(postal_code)
     except Exception as e:
-        if "is not a known country code" in str(e):  # Checks if the error is due to an unknown ISO code
-            print(f"The code {iso_code} has no available data. Please select a different ISO code.")
+        if "is not a known country code" in str(
+            e
+        ):  # Checks if the error is due to an unknown ISO code
+            print(
+                f"The code {iso_code} has no available data. Please select a different ISO code."
+            )
         else:
             print(f"An error occurred: {e}")
         return
-    
+
     lat = query.get("latitude")
     lon = query.get("longitude")
     city = query.get("place_name")
@@ -24,12 +28,10 @@ def locationer(postal_code, iso_code= "us"):
     ne_lat, sw_lat = round(lat + 0.14, 7), round(lat - 0.14, 7)
     ne_lon, sw_lon = round(lon + 0.14, 7), round(lon - 0.14, 7)
 
-    #print(f"The latitude is {lat} and the longitude is {lon} for the city {city}")
-    #print(f"The northeast bound is at {ne_lat}, {ne_lon} and the southwest bound is at {sw_lat}, {sw_lon}")
+    # print(f"The latitude is {lat} and the longitude is {lon} for the city {city}")
+    # print(f"The northeast bound is at {ne_lat}, {ne_lon} and the southwest bound is at {sw_lat}, {sw_lon}")
 
     return ne_lat, sw_lat, ne_lon, sw_lon
 
 
-
-
-#locationer(postal_code = postal_code, iso_code = iso_code)
+# locationer(postal_code = postal_code, iso_code = iso_code)
