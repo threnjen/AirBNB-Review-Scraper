d4cf3f595ba170db76907eb8fe08ea6a53adb5a6
threnjen <threnjen@gmail.com>
2025-10-27 15:32:35 -0700

    repo organizational cleanup

--- client_execution.py
+++ client_execution.py
@@ -1,28 +1,39 @@
 import json
-from scraper import airbnb_scraper
-from main import RagDescription
+import sys
+from scraper.scraper import airbnb_scraper
+from review_aggregator.review_aggregator import RagDescription
 
-with open("config.json", "r") as f:
-    config = json.load(f)
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    zipcode = config.get("zipcode", "00501")
+    iso_code = config.get("iso_code", "us")
+    scrape_reviews = config.get("scrape_reviews", False)
+    aggregate_reviews = config.get("agggregate_reviews", False)
+    number_of_listings_to_process = config.get("number_of_listings_to_process", 0)
 
-zipcode = config.get("zipcode", "00501")
-iso_code = config.get("iso_code", "us")
-scrape_reviews = config.get("scrape_reviews", False)
-aggregate_reviews = config.get("agggregate_reviews", False)
-number_of_listings_to_process = config.get("number_of_listings_to_process", 0)
+    print(f"Configuration loaded: {config}")
 
-if scrape_reviews == True:
-    airbnb_scraper(zipcode = zipcode, iso_code = iso_code, num_listings = number_of_listings_to_process)
-    print(f"Scraping reviews for zipcode {zipcode} in country {iso_code} completed.")
+    if scrape_reviews:
+        airbnb_scraper(
+            zipcode=zipcode,
+            iso_code=iso_code,
+            # num_listings=number_of_listings_to_process,
+        )
+        print(
+            f"Scraping reviews for zipcode {zipcode} in country {iso_code} completed."
+        )
 
-if aggregate_reviews == True:
-    rag_description = RagDescription()
-    rag_description.rag_description_generation_chain()
-    print(f"Aggregating reviews for zipcode {zipcode} in country {iso_code} completed.")
+    if aggregate_reviews:
+        rag_description = RagDescription()
+        rag_description.rag_description_generation_chain()
+        print(
+            f"Aggregating reviews for zipcode {zipcode} in country {iso_code} completed."
+        )
 
-# Things to do
-# Aggregrate the aggreated reviews into a single review per zip code
-# Simple frontend gui for config selection (use streamlit)
-# Turn the scraped data into a table for users to look at manually
-# Store output in aws in s3
-# Create a readme with copilot once done
+    # Things to do
+    # Aggregrate the aggreated reviews into a single review per zip code
+    # Simple frontend gui for config selection (use streamlit)
+    # Turn the scraped data into a table for users to look at manually
+    # Store output in aws in s3
+    # Create a readme with copilot once done
